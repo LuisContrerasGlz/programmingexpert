ee6b9815bb7a2ec68c4182945037d70127dcd3e9
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-10-06 17:26:13 -0500

    progress

--- Programming Fundamentals/conditionals.py
+++ Programming Fundamentals/conditionals.py
@@ -9,3 +9,19 @@ if age < 13:
     print("You can't ride the roller coaster.")
 else:
     print("You can ride the roller coaster!")
+
+"""
+Write a program that asks the user to input their favorite programming
+language and outputs a specific string based on their answer.
+"""
+
+fav_language = input("What's your favorite programming language? ")
+
+if fav_language == "Python":
+    print("Nice choice!")
+elif fav_language == "Golang":
+    print("You're a cool kid I see...")
+elif fav_language == "JavaScript":
+    print("Okay Mr. web developer.")
+else:
+    print("I don't know that language.")

2caf967d61a0db1fcdd3abb154749a9a02d5ffd3
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-10-06 17:30:36 -0500

    progress

--- Programming Fundamentals/Conditions.py
+++ Programming Fundamentals/Conditions.py
@@ -46,3 +46,15 @@ print(condition_3)
 print(condition_4)
 print(condition_5)
 
+# Numbers game program
+
+number1 = float(input("Enter a number: "))
+
+if number1 >= 10 and number1 <= 20:
+    number2 = float(input("Enter another number: "))
+    sum_of_numbers = number1 + number2
+
+    print("The sum of these two numbers is:", sum_of_numbers)
+
+    if sum_of_numbers > 100:
+        print("That is a large sum!")

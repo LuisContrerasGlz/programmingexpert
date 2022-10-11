de956c53012221c3742b4792e04e3fac9f4bf5a5
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-10-11 01:24:58 -0500

    progress

--- Programming Fundamentals/lists.py
+++ Programming Fundamentals/lists.py
@@ -1,10 +1,12 @@
 """
+
  1. Write a program that asks the user to input five strings and stores them in a list.
  
     Then, the program should ask the user to input three numbers in the range of 0 and 4 inclusive (these numbers will represent list indices).
  
     Finally, the program should use these numbers to access the three strings at the corresponding indices in the previously created list, concatenate them,
     and print out the resulting string.
+    
 """
 
 string1 = input("Enter a string: ")

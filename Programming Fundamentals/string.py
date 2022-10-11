2c551b1491900be56021e9133ebe553e17676e12
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-10-11 01:04:08 -0500

    progress

--- Programming Fundamentals/string.py
+++ Programming Fundamentals/string.py
@@ -15,4 +15,18 @@ if games.isdigit():
 else:
     print(games.capitalize())
 
+"""
+
+2. Write a program that asks the user to input two words and determines if the first word is contained in the second one.
+
+"""
+
+
+first_word = input("Enter a word: ")
+second_word = input("Enter another word: ")
+
+if first_word in second_word:
+    print("The first word is contained in the second one")
+else:
+    print("The first word isn't contained in the second one")
 

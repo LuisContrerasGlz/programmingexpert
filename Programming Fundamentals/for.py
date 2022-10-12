6502d3502767788ae300ccea2f0ef7111bde687a
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-10-11 20:20:31 -0500

    progress

--- Programming Fundamentals/for.py
+++ Programming Fundamentals/for.py
@@ -50,3 +50,17 @@ for idx in range(len(string1)):
 
     if character1 == character2:
         print(character1)
+
+"""
+
+4. Use a single for loop to iterate through the provided list, 
+   and print the elements that are both divisible by 2 and located at
+   an odd index, each on a separate line.
+
+"""
+
+lst = [45, 24, 22, 1, 45, 2, 12, 13, 16, 10, 0, -7]
+
+for i in range(len(lst)):
+    if (lst[i] % 2 == 0) and (i % 2 == 1):
+        print(lst[i]) 

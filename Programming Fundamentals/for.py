be52b3e3cb0f6b09872d590009989076e599a88d
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-10-11 20:14:48 -0500

    progress

--- Programming Fundamentals/for.py
+++ Programming Fundamentals/for.py
@@ -30,3 +30,23 @@ for character in string:
 
 for item in tupl:
     print(item)
+
+
+"""
+
+3. Use a single for loop to iterate through the two provided
+    strings, and print all of their matching characters (i.e., the characters
+    that are at the same index and that are equal to each other), each on a
+    separate line.
+
+"""
+
+string1 = "aabbcsdw"
+string2 = "abbbcsdd"
+
+for idx in range(len(string1)):
+    character1 = string1[idx]
+    character2 = string2[idx]
+
+    if character1 == character2:
+        print(character1)

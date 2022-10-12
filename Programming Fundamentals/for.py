7d8e12a154a348d0d11bba08fdba986f5d1fe972
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-10-11 20:04:04 -0500

    progress

--- Programming Fundamentals/for.py
+++ Programming Fundamentals/for.py
@@ -6,3 +6,27 @@
 
 for i in range(1,11):
     print(i)
+
+
+"""
+
+2. User multiple for loops to iterate through the provided list,
+   string, and tuple, and print all of their elements, each on a separate line.
+
+   First print all of the list items, then all of the string characters, then
+   all of the tuple items.
+
+"""
+
+lst = ["tim", "is", "the", "best", "instructor"]
+string = "..."
+tupl = ("and", "he", "is", "great")
+
+for item in lst:
+    print(item)
+
+for character in string:
+    print(character)
+
+for item in tupl:
+    print(item)

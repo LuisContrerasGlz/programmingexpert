d020a102ca6f81aef5ab2ec3134437b72e39f67d
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-11-04 21:12:40 -0600

    progress

--- functions.py
+++ functions.py
@@ -7,3 +7,20 @@ Your function should add the two numbers and return the result.
 
 def add(x,y):
     return x + y
+
+"""
+
+Write a funcion find_all_odds(lst), 
+which takes in a list of integers and returns a new list containing all of the odd numbers of the
+original list, in the order in which they appear. You can assume that lst will only contain integers.
+
+"""
+
+def find_all_odds(lst):
+    lst2 = [] 
+    
+    for i in lst:
+        if i % 2 == 1:
+            lst2.append(i)
+
+    return lst2

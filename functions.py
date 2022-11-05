7860d05fabb4173d404f842c42f6ea21fefa31eb
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-11-04 22:15:47 -0600

    progress

--- functions.py
+++ functions.py
@@ -62,3 +62,22 @@ def compare_lists(lst1=[], lst2=[]):
     set_intersection = lst1_set.intersection(lst2_set)
 
     return len(set_intersection)
+
+
+"""
+
+    Write the function trim_list(lst, elements_to_trim)
+    Which takes in a list and returns a new version of the input list where the last elements_to_trim elements have been removed. 
+    You can assume that elements_to_trim will always be a positive integer that's
+    smaller than the length of lst.
+
+"""
+
+def trim_list(lst, elements_to_trim):
+    trimmed_list = []
+
+    for idx in range(len(lst) - elements_to_trim):
+        element = lst[idx]
+        trimmed_list.append(element)
+
+    return trimmed_list

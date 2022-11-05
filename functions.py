6c6d4b991c01a3291b521bff51773782d945796a
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-11-04 21:04:01 -0600

    progress

---
+++ functions.py
@@ -0,0 +1,9 @@
+"""
+
+Write a function named add that takes in two parameters x and y, both expected to be numbers. 
+Your function should add the two numbers and return the result.
+
+"""
+
+def add(x,y):
+    return x + y

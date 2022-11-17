34653be403fe4c1850ae41775f49afcf7dc4dbcf
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-11-17 02:01:45 -0600

    progress

---
+++ Programming Fundamentals/Assessments/Random Number Guesser.py
@@ -0,0 +1,21 @@
+"""
+
+    Write a program that asks the user to enter two integers representing the
+    start and the end of a range. The program should then generate a random
+    number within this range (inclusively) and ask the user to guess numbers
+    until they guess the randomly generated number. Once the user guesses the
+    random number, the program should tell them how many attempts it took them
+    to guess it.
+
+    Your program needs to ensure that the range of numbers given is valid. 
+    For example, if the user enters a number for the end of the range that is less
+    than the start of the range your program needs ask them to enter a valid
+    number. Your program must also handle any other errors that might occur,
+    like the user entering a string instead of an integer.
+
+    Note: You may assume the start of the range will never be negative (i.e you
+    don't need to handle negative values).
+
+    
+
+"""

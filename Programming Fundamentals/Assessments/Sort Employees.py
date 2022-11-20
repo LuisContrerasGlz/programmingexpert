c89f6d2d029e9878f6add1109d331a677f2dc811
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-11-20 01:47:02 -0600

    progress

---
+++ Programming Fundamentals/Assessments/Sort Employees.py
@@ -0,0 +1,18 @@
+"""
+
+  Write a function that accepts a list of lists that contain the name, age and
+  salary of specific employees and also accepts a string representing the key to
+  sort employees by. Your function should return a new list that contains the
+  lists representing each employee sorted in ascending order by the given key.
+
+
+
+"""
+
+def sort_employees(employees, sort_by):
+    sort_indices = ["name", "age", "salary"]
+    sort_index = sort_indices.index(sort_by)
+
+    sorted_employees = sorted(employees, key=lambda x: x[sort_index])
+
+    return sorted_employees

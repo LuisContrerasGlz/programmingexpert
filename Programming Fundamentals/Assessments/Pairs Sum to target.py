fd3c30bc056cdb8deb76f01e449f83148cfb6fdd
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-11-23 00:19:32 -0600

    progress

---
+++ Programming Fundamentals/Assessments/Pairs Sum to target.py
@@ -0,0 +1,18 @@
+"""
+
+    Write a function that accepts two lists (list1 and list2) of integers and a target integer named target.
+    Your function should return all pairs of indices in the form [x,y] where list1[x] + list2[y] == target.
+    In other words, return the pairs of indices where the sum of their values equals target.
+    lis1 and lis2 will always have the same number of elements and you may return the pairs in any order.
+
+"""
+
+def pairs_sum_to_target(list1, list2, target):
+    pairs = []
+
+    for i, value1 in enumerate(list1):
+        for j, value2 in enumerate(list2):
+            if value1 + value2 == target:
+                pairs.append([i, j])
+
+    return pairs

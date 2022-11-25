9c1047671357da5e347145c35011cd34785a277b
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-11-25 17:17:07 -0600

    progress

--- Programming Fundamentals/Assessments/Longest Unique Words.py
+++ Programming Fundamentals/Assessments/Longest Unique Words.py
@@ -12,3 +12,17 @@
     or spaces.
 
 """
+
+def get_n_longest_unique_words(words, n):
+    unique_words = get_unique_words(words)
+    sorted_words = sorted(unique_words, key=len, reverse=True)
+    return sorted_words[:n]
+
+
+def get_unique_words(words):
+    unique_words = []
+    for word in words:
+        if words.count(word) == 1:
+            unique_words.append(word)
+
+    return unique_words

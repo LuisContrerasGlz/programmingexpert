d24dba393300ec010f94c482a5f35cdf63039a23
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-11-26 01:27:45 -0600

    progress

--- Programming Fundamentals/Assessments/Longest Unique Words.py
+++ Programming Fundamentals/Assessments/Longest Unique Words.py
@@ -14,15 +14,27 @@
 """
 
 def get_n_longest_unique_words(words, n):
+
+    # Using the get_unique_words function to create the list from the parameter
     unique_words = get_unique_words(words)
+
+    # We sort the words using the len, and reverse the order so it is correct in decending order
     sorted_words = sorted(unique_words, key=len, reverse=True)
+
+    # We return a slice of the list up to n
     return sorted_words[:n]
 
+# Defining a funtion to check the unique words of the list
 
 def get_unique_words(words):
+    # Declaring an empty list called unique words
+
     unique_words = []
+    # Loopting the list and checking if the count of the words is 1, it it is we append it to the list
+
     for word in words:
         if words.count(word) == 1:
             unique_words.append(word)
 
+    # We return the unique words 
     return unique_words

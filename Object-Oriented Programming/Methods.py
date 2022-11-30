5d292a6d48500df24e8d2100f41378fc65f81141
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-11-30 15:52:11 -0600

    progress

--- Object-Oriented Programming/Methods.py
+++ Object-Oriented Programming/Methods.py
@@ -1,19 +1,23 @@
 # Problem 1
 
 class Rectangle:
+    # Defining the contructor of the Rectangle
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
 
+    # Defining a method to change the position of x and y with new values
     def change_position(self, x, y):
         self.x = x
         self.y = y
 
+    # Defining get position method which returns x and y in a tuple 
     def get_position(self):
         return (self.x, self.y)
 
+    # Defining the get area method which will use the width and height attributes to calculate and return the area
     def get_area(self):
         return self.width * self.height
 

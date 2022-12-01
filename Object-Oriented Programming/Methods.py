b83e138115b0adc0ce8aa06f568acf60b7e45eaf
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-11-30 23:55:37 -0600

    progress

--- Object-Oriented Programming/Methods.py
+++ Object-Oriented Programming/Methods.py
@@ -28,14 +28,17 @@ class Group:
         self.name = name
         self.members = members
 
+    # Declaring a method add to add a member to the members list using append
     def add(self, name):
         self.members.append(name)
 
+    # Declaring the delete method to check if a name if already in members, if it is there we remove it with the .remove but if it is not there we raise an exception
     def delete(self, name):
         if name in self.members:
             self.members.remove(name)
         else:
             raise Exception("Member not in group.")
 
+    # Declaring a get members function to return the order members, we use sorted() so we dont modify the original one, instead getting a new one
     def get_members(self):
         return sorted(self.members)

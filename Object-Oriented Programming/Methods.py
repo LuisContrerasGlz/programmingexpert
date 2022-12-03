fbba47cef783ca4d1b650291dd24bfc0d8ea81a5
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-12-03 02:13:07 -0600

    progress

--- Object-Oriented Programming/Methods.py
+++ Object-Oriented Programming/Methods.py
@@ -92,3 +92,24 @@ class BankAccount:
 
         # If all the conditions are good we have selt._balance been what was passed in there
         self._balance = balance
+
+# Problem 5 
+
+class BankAccount:
+    def __init__(self, account_holder_name):
+        self.account_holder_name = account_holder_name
+        self._balance = 0.0
+
+    @property
+    def balance(self):
+        return round(self._balance)
+
+    @balance.setter
+    def balance(self, balance):
+        if type(balance) not in [int, float]:
+            return
+
+        if balance < 0 or balance > 100000:
+            return
+
+        self._balance = balance

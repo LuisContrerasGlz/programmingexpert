08151607caa003f524b2b24515e8d9843b8c0377
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-12-03 23:44:15 -0600

    progress

--- Object-Oriented Programming/ClassMethodsAndAttributes.py
+++ Object-Oriented Programming/ClassMethodsAndAttributes.py
@@ -1,3 +1,5 @@
+# Problem 1 
+
 class Person:
   population = 0
 
@@ -11,3 +13,21 @@ p1 = Person("Tim", 100)
 p2 = Person("Clement", 54)
 
 x = Person.population
+
+# Problem 2
+
+class Employee:
+    number_of_employees = 0
+    average_age = 0
+    average_salary = 0
+
+    def __init__(self, name, age, salary):
+        self.name = name
+        self.age = age
+        self.salary = salary
+
+        total_age = Employee.average_age * Employee.number_of_employees
+        total_salary = Employee.average_salary * Employee.number_of_employees
+        Employee.average_age = (total_age + age) / (Employee.number_of_employees + 1)
+        Employee.average_salary = (total_salary + salary) / (Employee.number_of_employees + 1)
+        Employee.number_of_employees += 1

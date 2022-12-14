c66f8e7b52593ac04727c9a283a18278188bb3b9
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-12-14 16:00:42 -0600

    progress

--- reverse_String.py
+++ reverse_String.py
@@ -1,5 +1,8 @@
 # Reverse a string
 
+# Slice will give us the elements back, we select -1 in the step in order to do that
+# Start(Inclusive):Stop(Exclusive):Step
+
 def rev_sting(x):
   return x[::-1]
 

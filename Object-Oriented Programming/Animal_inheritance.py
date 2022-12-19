b0702cb1f54b079deadc9a787e1f047dfdd0a556
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-12-19 17:06:16 -0600

    progress

--- Object-Oriented Programming/Animal_inheritance.py
+++ Object-Oriented Programming/Animal_inheritance.py
@@ -10,3 +10,37 @@ All Lizards have a tail and also have a color
 Your classes should represent this.
 
 """
+
+class Animal:
+    def __init__(self, age, weight, height):
+        self.age = age
+        self.weight = weight
+        self.height = height
+
+
+class Mammal(Animal):
+    def __init__(self, age, weight, height, sex):
+        super().__init__(age, weight, height)
+        self.sex = sex
+
+
+class Reptile(Animal):
+    def __init__(self, age, weight, height, legs):
+        super().__init__(age, weight, height)
+        self.legs = legs
+
+
+class Monkey(Mammal):
+    fingers = 5
+
+    def __init__(self, age, weight, height, sex, color):
+        super().__init__(age, weight, height, sex)
+        self.color = color
+
+
+class Lizard(Reptile):
+    tail = True
+
+    def __init__(self, age, weight, height, legs, color):
+        super().__init__(age, weight, height, legs)
+        self.color = color

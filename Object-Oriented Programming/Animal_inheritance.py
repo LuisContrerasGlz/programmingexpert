1096a5e718c3b24bc9a705bd1a352d6393f352a9
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2022-12-19 17:05:23 -0600

    progress

---
+++ Object-Oriented Programming/Animal_inheritance.py
@@ -0,0 +1,12 @@
+"""
+Create an Animan, Mamal and Reptile class as defined below. 
+Then create a Monkey and Lizard class that inherit from the proper base classes.
+
+All Mammals and Reptiles are Animals; your inheritance should reflect this.
+All Animals have an age weight and height. All Mammals have a sex and most Reptiles have 4 legs but no all.  Your classes should represent this.
+
+All Monkeys have exactly 5 fingers and a color.
+All Lizards have a tail and also have a color
+Your classes should represent this.
+
+"""

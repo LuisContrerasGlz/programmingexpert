b17a5f648857c057d547b6aa27b2a3087c071c36
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2023-01-02 14:58:05 -0600

    Progress

--- Simple_ExtraPractice_Problems/change_positions_shortcutAndSort.py
+++ Simple_ExtraPractice_Problems/change_positions_shortcutAndSort.py
@@ -13,3 +13,20 @@ print("After swapping: ", first, second)
 top_cities = ["New York", "Los Angeles", "Chicago", "Phoenix"]
 top_cities[0], top_cities[4] = top_cities[4], top_cities[0]
 print(top_cities)
+
+# Sorting the list
+
+top_cities.sort()
+
+# Sorting numeric list
+
+random_numbers = [2,5,0,-3,4]
+random_numbers.sort()
+
+# Reversing with keyword to be reverse
+random_numbers.sort(reverse=True)
+
+# Now using the sorted function
+
+print(sorted(top_cities))
+
---
+++ sort_And_sorted.py
@@ -0,0 +1,23 @@
+top_cities = ["New York", "Los Angeles", "Chicago", "Phoenix"]
+
+# Sorting the list
+
+top_cities.sort()
+
+# Sorting numeric list
+
+random_numbers = [2,5,0,-3,4]
+random_numbers.sort()
+
+# Reversing with keyword to be reverse
+random_numbers.sort(reverse=True)
+
+# Reversing with keyword to be reverse
+random_numbers = [2,5,0,-3,4]
+random_numbers.sort(reverse=True)
+
+# Now using the sorted function
+
+print(sorted(top_cities))
+
+# Difference between list_name.sort and sorted(list_name) is that sorted() creates a new list and .sort() sorts the original list

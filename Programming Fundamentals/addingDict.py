54f807901c569d39cf2c4253dad9a299e9f19685
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2023-01-02 13:52:45 -0600

    progress

---
+++ Programming Fundamentals/addingDict.py
@@ -0,0 +1,24 @@
+# To create an empty dictionary in Python, you can use the dict() function with no arguments:
+
+my_dict = dict()
+
+# You can also use the curly braces {} to create an empty dictionary:
+
+my_dict = {}
+
+# Once you have created an empty dictionary, you can add key-value pairs to it using the square brackets [] notation:
+
+my_dict = {}
+
+# Add a key-value pair to the dictionary
+my_dict["key"] = "value"
+
+# Add another key-value pair to the dictionary
+my_dict["key2"] = "value2"
+
+# You can also use the update() method to add multiple key-value pairs to a dictionary at once:
+
+my_dict = {}
+
+# Add multiple key-value pairs to the dictionary
+my_dict.update({"key": "value", "key2": "value2"})

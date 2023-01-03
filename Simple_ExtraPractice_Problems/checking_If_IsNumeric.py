daa7e9844fdfdc09812c09cef5db11ac1f3db596
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2023-01-03 01:47:21 -0600

    Progress

---
+++ Simple_ExtraPractice_Problems/checking_If_IsNumeric.py
@@ -0,0 +1,8 @@
+# Using the .isnumeric() string method 
+
+user_number = input("Please provide a number: ")
+
+if user_number.isnumeric():
+    print("Thank you, that is correct!")
+else:
+    print("Sorry, ", user_number, "is not a number")

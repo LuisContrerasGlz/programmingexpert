37f02eb059c83fa399ddb1b2389d37b6f75e32fe
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2023-01-03 01:36:06 -0600

    Progress

--- Simple_ExtraPractice_Problems/nested_lists.py
+++ Simple_ExtraPractice_Problems/nested_lists.py
@@ -1,7 +1,8 @@
 # Declaring a nested list
-cells = [["A1", "A2", "A3"], ["B1","B2","B3"]]
+table = [["A1", "A2", "A3"], ["B1","B2","B3"]]
 
 # Using nested for loop to iterate the lists 
-for x in cells:
-    for y in x:
-        print("Element: ", y)
+for row in table:
+    for cell in row:
+        print("Element: ", cell)
+

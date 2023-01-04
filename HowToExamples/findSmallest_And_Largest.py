3d29438c5cd425eb9aca6eab3ebdb6b6a3c3fbbe
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2023-01-04 14:40:58 -0600

    progress

--- HowToExamples/findSmallest_And_Largest.py
+++ HowToExamples/findSmallest_And_Largest.py
@@ -1,5 +1,3 @@
-# Find the smallest and largest in a list
-
 # Find the smallest and largest element in a list
 numbers = [3, 7, 2, 8, 4]
 smallest = min(numbers)
@@ -17,3 +15,24 @@ longest = max(words, key=len)
 
 print(f"Shortest word: {shortest}")
 print(f"Longest word: {longest}")
+
+"""
+
+To find the second smallest and second largest elements in a list in Python, 
+we can use the sorted() function to sort the list, 
+and then use indexing to get the second smallest and second largest elements.
+
+"""
+
+# Find the second smallest and second largest element in a list
+numbers = [3, 7, 2, 8, 4]
+sorted_numbers = sorted(numbers)
+
+second_smallest = sorted_numbers[1]
+second_largest = sorted_numbers[-2]
+
+print(f"Second smallest number: {second_smallest}")
+print(f"Second largest number: {second_largest}")
+
+
+

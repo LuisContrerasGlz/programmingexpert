f11950540d575d2afcbc41b973942354f5717dbb
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2023-01-05 14:50:13 -0600

    Progress

---
+++ Simple_ExtraPractice_Problems/oddNumsPrint.py
@@ -0,0 +1,14 @@
+# Create a for loop that counts from 0 to 10, and prints odd numbers to the screen. 
+
+for i in range(0, 11):
+    if i % 2 != 0:
+        print(i)
+
+# Using a while
+
+x = 1
+while x < 11:
+    if x % 2 != 0:
+        print(x)
+    x += 1
+

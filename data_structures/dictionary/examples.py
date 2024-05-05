1bdc6b2701b20a56f06e7c9dc4059bfc1bc00a02
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-05-04 18:34:18 -0600

    data structures

--- data_structures/dictionary/examples.py
+++ data_structures/dictionary/examples.py
@@ -5,6 +5,16 @@ car =	{
   "model": "Mustang",
   "year": 1964
 }
-print(
-car.get("model")
-)
+
+print(car.get("model"))
+
+
+# Change the "year" value from 1964 to 2020.
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+car["year"] = 2020

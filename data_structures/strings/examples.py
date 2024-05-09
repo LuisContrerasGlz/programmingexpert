85dee50e21ca848f244abd807897f8c9c63e59ff
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-05-09 16:21:59 -0600

    data structures

--- data_structures/strings/examples.py
+++ data_structures/strings/examples.py
@@ -7,3 +7,26 @@ print(len(x))
 
 txt = "Hello World"
 x = txt[0]
+
+# Get the characters from index 2 to index 4 (llo).
+
+txt = "Hello World"
+x = txt[2:5]
+
+# Return the string without any whitespace at the beginning or the end.
+
+txt = " Hello World "
+x2 = txt.strip()
+print(x2)
+
+# return a string without any blank space in python
+
+original_str = "Hello World! This is a test."
+no_spaces_str = original_str.replace(" ", "")
+print(no_spaces_str)  # Output: "HelloWorld!Thisisatest."
+
+original_str = "Hello World! This is a test."
+no_spaces_str = "".join(original_str.split())
+print(no_spaces_str)  # Output: "HelloWorld!Thisisatest."
+
+

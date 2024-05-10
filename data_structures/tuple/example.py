6f6ce357bb1b31c798cfd8faa24302d08400f617
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-05-10 15:13:20 -0600

    progress

--- data_structures/tuple/example.py
+++ data_structures/tuple/example.py
@@ -12,3 +12,8 @@ print(len(fruits))
 
 fruits = ("apple", "banana", "cherry")
 print(fruits[-1])
+
+# Use a range of indexes to print the third, fourth, and fifth item in the tuple.
+
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])

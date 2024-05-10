f0a840fcb1aeaa8afe36d98298d400aa6aa5fb8b
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-05-09 19:18:05 -0600

    data structures

--- data_structures/list/example.py
+++ data_structures/list/example.py
@@ -24,3 +24,17 @@ fruits.insert(1,"lemon")
 fruits = ["apple", "banana", "cherry"]
 fruits.remove("banana")
 
+# Use negative indexing to print the last item in the list.
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+# Use a range of indexes to print the third, fourth, and fifth item in the list.
+
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+# Use the correct syntax to print the number of items in the list.
+
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))

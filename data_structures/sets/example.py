2d8ee6ffea4709bd76ec6f12e873bb92dee9874a
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-05-10 15:16:44 -0600

    progress

---
+++ data_structures/sets/example.py
@@ -0,0 +1,12 @@
+# Check if "apple" is present in the fruits set.
+
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits: 
+    print("Yes, apple is a fruit!")
+
+
+# Use the add method to add "orange" to the fruits set.
+    
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+

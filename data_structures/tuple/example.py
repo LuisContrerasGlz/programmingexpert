216bbd5aea427e70c06f77e42188efb48924a2f9
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-05-10 15:11:58 -0600

    progress

---
+++ data_structures/tuple/example.py
@@ -0,0 +1,9 @@
+# Use the correct syntax to print the first item in the fruits tuple.
+
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+
+# Use the correct syntax to print the number of items in the fruits tuple.
+
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))

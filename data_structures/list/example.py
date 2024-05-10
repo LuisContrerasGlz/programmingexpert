cb3f0ff90097148b94e75e896adfbca5069e6bb8
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-05-09 19:13:54 -0600

    data structures

---
+++ data_structures/list/example.py
@@ -0,0 +1,11 @@
+# Print the second item in the fruits list.
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+# Change the value from "apple" to "kiwi", in the fruits list.
+
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+
+

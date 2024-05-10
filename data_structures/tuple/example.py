fc33e19a411610208c371fe575dcab979fe5e550
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-05-10 15:12:40 -0600

    progress

--- data_structures/tuple/example.py
+++ data_structures/tuple/example.py
@@ -7,3 +7,8 @@ print(fruits[0])
 
 fruits = ("apple", "banana", "cherry")
 print(len(fruits))
+
+# Use negative indexing to print the last item in the tuple.
+
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])

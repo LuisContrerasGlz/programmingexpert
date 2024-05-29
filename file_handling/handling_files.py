45419fdbe1084a20ed7560918420cddb36b29a2a
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-05-28 22:50:24 -0600

    progress

--- file_handling/handling_files.py
+++ file_handling/handling_files.py
@@ -78,4 +78,18 @@ f.close()
 f = open("demofile3.txt", "r")
 print(f.read())
 
+"""
 
+To create a new file in Python, use the open() method, with one of the following parameters:
+
+"x" - Create - will create a file, returns an error if the file exist
+
+"a" - Append - will create a file if the specified file does not exist
+
+"w" - Write - will create a file if the specified file does not exist
+
+"""
+
+f = open("myfile.txt", "x")
+
+f = open("myfile.txt", "w")

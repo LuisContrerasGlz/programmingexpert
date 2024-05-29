93a14c9f3e19fd1d3149abd9f981b614eef7e93e
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-05-28 21:45:05 -0600

    add json

--- JSON/example_json.py
+++ JSON/example_json.py
@@ -11,3 +11,18 @@ y = json.loads(x)
 # the result is a Python dictionary:
 print(y)
 print(y["age"])
+
+# Convert from Python to JSON
+
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)

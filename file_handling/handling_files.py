5e17b5e5b63c8252aef7a1b28baef6521d3287e7
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-05-28 22:48:55 -0600

    progrss

--- file_handling/handling_files.py
+++ file_handling/handling_files.py
@@ -51,3 +51,31 @@ f = open("demofile.txt", "r")
 print(f.readline())
 f.close()
 
+"""
+To write to an existing file, you must add a parameter to the open() function:
+
+"a" - Append - will append to the end of the file
+
+"w" - Write - will overwrite any existing content
+
+"""
+
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
+#open and read the file after the appending:
+f = open("demofile2.txt", "r")
+print(f.read())
+
+# Open the file "demofile3.txt" and overwrite the content:
+
+f = open("demofile3.txt", "w")
+f.write("Woops! I have deleted the content!")
+f.close()
+
+#open and read the file after the overwriting:
+f = open("demofile3.txt", "r")
+print(f.read())
+
+

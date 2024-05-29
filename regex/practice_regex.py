99114767b1a70d76f52af811dbf6b1a4f3ce16c9
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-05-28 22:19:13 -0600

    add json

--- regex/practice_regex.py
+++ regex/practice_regex.py
@@ -56,4 +56,16 @@ txt = "The rain in Spain"
 x = re.split("\s", txt)
 print(x)
 
+# Replace every white-space character with the number 9:
+
+txt = "The rain in Spain"
+x = re.sub("\s", "9", txt)
+print(x)
+
+# Replace the first 2 occurrences:
+
+txt = "The rain in Spain"
+x = re.sub("\s", "9", txt, 2)
+print(x)
+
 

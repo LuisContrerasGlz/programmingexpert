91634cc026b0c32c6e8f8d052e4053e8645eade1
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-06-15 14:35:09 -0600

    progress

---
+++ W3/reverse_string.py
@@ -0,0 +1,12 @@
+"""
+
+How to Reverse a String in Python
+
+There is no built-in function to reverse a String in Python.
+
+The fastest (and easiest?) way is to use a slice that steps backwards, -1.
+
+"""
+
+txt = "Hello World"[::-1]
+print(txt)

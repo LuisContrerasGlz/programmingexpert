e95ed4b50e382d3e3fc687d49e49b1692336d816
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-06-15 14:40:12 -0600

    progress

--- W3/reverse_string.py
+++ W3/reverse_string.py
@@ -19,3 +19,22 @@ def my_function(x):
 mytxt = my_function("I wonder how this text looks like backwards")
 
 print(mytxt)
+
+# Using a for loop
+
+def reverse_string(input_string):
+    # Initialize an empty string to store the reversed string
+    reversed_string = ""
+    # Loop through the input string in reverse order
+    for i in input_string:
+        # Prepend each character to the reversed_string
+        reversed_string = i + reversed_string
+    return reversed_string
+
+# Example usage
+original_string = "Hello, World!"
+reversed_string = reverse_string(original_string)
+print("Original string:", original_string)
+print("Reversed string:", reversed_string)
+
+

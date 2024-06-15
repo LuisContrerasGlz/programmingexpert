1e5ac55179ffc3915c5408eddd7307a9339764c2
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-06-15 14:44:08 -0600

    progress

---
+++ W3/reverse_words_from_string.py
@@ -0,0 +1,25 @@
+"""
+Reverse the worlds in a string
+
+1. Split the string into words(making it a list).
+2. Reverse the list of words.
+3. Join the reversed list of words back into a string.
+
+"""
+
+def reverse_words(input_string):
+    # Split the input string into words
+    words = input_string.split()
+    # Reverse the list of words
+    reversed_words = words[::-1]
+    # Join the reversed list of words back into a string
+    reversed_string = ' '.join(reversed_words)
+    return reversed_string
+
+# Example usage
+original_string = "Hello, World! How are you?"
+reversed_string = reverse_words(original_string)
+print("Original string:", original_string)
+print("Reversed words string:", reversed_string)
+
+
